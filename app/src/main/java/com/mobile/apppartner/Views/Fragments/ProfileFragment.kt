f5528feab66d0ad1c63437b27eaea621f545be0b
Josue <josuegomez1994@hotmail.com>
2019-05-21 22:37:37 -0500

    MODIFY PROFILEFRAGMENT

--- app/src/main/java/com/mobile/apppartner/Views/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/mobile/apppartner/Views/Fragments/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.example.appprueba
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -18,28 +19,35 @@ class ProfileFragment: Fragment() {
     lateinit var viewModel: ProfileViewModel
     var us:UserDatabase?=null
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getUser(this).subscribe({
+            us=it
+            setValues()
+        },{
+            print(it.toString())
+        })
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile,container,false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
 
 
-        viewModel.getUser(this).subscribe({
-            setValues()
-        },{
-            print(it.toString())
-        })
+
+
 
         this.btnSalir.setOnClickListener {
             viewModel.signOut(activity!!)
         }
     }
     fun setValues(){
-        this.txtNombrePR.setText(viewModel.u?.fullname.toString())
-        this.txtDescripcionPR.setText(viewModel.u?.descripcion.toString())
-        Picasso.get().load(viewModel.u?.url_img.toString()).transform(CircleTransformation()).into(this.imgPerfilPR)
+        this.txtNombrePR.setText(us?.fullname.toString())
+        this.txtDescripcionPR.setText(us?.descripcion.toString())
+        Picasso.get().load(us?.url_img.toString()).transform(CircleTransformation()).into(this.imgPerfilPR)
     }
 }
